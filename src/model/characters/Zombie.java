cfaf307abe4af3cc1b7b0632f73cc003e9961521
RasheedAtia <rasheedatia@hotmail.com>
2023-03-17 16:47:38 +0200

    Update Zombie.java

--- src/model/characters/Zombie.java
+++ src/model/characters/Zombie.java
@@ -1,5 +1,11 @@
 package model.characters;
 
+/**
+ * A class representing zombies in the game.
+ * @author Ahmed, Mostafa, Rasheed
+ *
+ */
+
 public class Zombie extends Character{
 	private static int ZOMBIES_COUNT = 0;
 	
