b82923183d6c145e314d36cf010967c15413faae
RasheedAtia <rasheedatia@hotmail.com>
2023-03-17 16:24:04 +0200

    Hero subclasses created
    fighter
    medic
    explorer

---
+++ src/model/characters/Fighter.java
@@ -0,0 +1,15 @@
+package model.characters;
+
+/**
+ * A class representing fighters in the game.
+ * @author Ahmed, Mostafa, Rasheed
+ *
+ */
+
+public class Fighter extends Hero{
+
+	public Fighter(String name, int maxHp, int attackDmg, int maxActions) {
+		super(name, maxHp, attackDmg, maxActions);
+	}
+	
+}
