524f8ae71dbfdafa558c3730a5adab06ea0a970e
RasheedAtia <rasheedatia@hotmail.com>
2023-03-17 16:01:33 +0200

    Character
    class created

---
+++ src/model/characters/Character.java
@@ -0,0 +1,59 @@
+package model.characters;
+import java.awt.Point;
+
+/**
+ * An abstract class representing characters in the game.
+ * @author Ahmed, Mostafa, Rasheed
+ *
+ */
+
+public abstract class Character {
+	private String name;
+	private Point location;
+	private int maxHp;
+	private int currentHp;
+	private int attackDmg;
+	private Character target;
+	
+	public Character(String name, int maxHp, int attackDmg) {
+		this.name = name;
+		this.maxHp = maxHp;
+		this.attackDmg = attackDmg;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public Point getLocation() {
+		return this.location;
+	}
+	
+	public void setLocation(Point location) {
+		this.location = location;
+	}
+	
+	public int getMaxHp() {
+		return this.maxHp;
+	}
+	
+	public int getCurrentHp() {
+		return this.currentHp;
+	}
+	
+	public void setCurrentHp(int currentHp) {
+		this.currentHp = currentHp;
+	}
+	
+	public int getAttackDmg() {
+		return this.attackDmg;
+	}
+	
+	public Character getTarget() {
+		return this.target;
+	}
+	
+	public void setTarget(Character target) {
+		this.target = target;
+	}
+}
---
+++ src/model/characters/Hero.java
@@ -0,0 +1,11 @@
+package model.characters;
+
+/**
+ * An abstract class representing characters in the game.
+ * @author Ahmed, Mostafa, Rasheed
+ *
+ */
+
+public abstract class Hero extends Character{
+	
+}
