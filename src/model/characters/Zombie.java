5adf414ae109dec7c46e83f86148b3f071758853
Mostafa Hamdy <mostaaf.hamdy@student.guc.edu.eg>
2023-03-20 19:03:28 +0200

    fixed zombie constructor

--- src/model/characters/Zombie.java
+++ src/model/characters/Zombie.java
@@ -10,7 +10,7 @@ public class Zombie extends Character{
 	private static int ZOMBIES_COUNT = 0;
 	
 	public Zombie() {
-		super("Zombie" + ++ZOMBIES_COUNT, 40, 10);
+		super("Zombie " + ++ZOMBIES_COUNT, 40, 10);
 	}
 
 }
