4a002e1deed6eae75ed5debbb7e559315e575758
Mostafa Hamdy <mostaaf.hamdy@student.guc.edu.eg>
2023-03-20 20:11:09 +0200

    edited setSafe and setCharacter

--- src/model/world/CharacterCell.java
+++ src/model/world/CharacterCell.java
@@ -15,15 +15,14 @@ public class CharacterCell extends Cell{
 	public Character getCharacter() {
 		return character;
 	}
-	public void setCharacter(Character c) {
-		character.setCurrentHp(c.getCurrentHp());
+	public void setCharacter(Character character) {
+		this.character=character;
 	} 
 	public boolean isSafe() {
 		return isSafe;
 	}
 	public void setSafe(boolean isSafe) {
 		this.isSafe = isSafe;
-		setVisible(isSafe);
 	}
 	
 }
