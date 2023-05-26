cbe2177108c660427f5569b8fbc10f8b51a5ff5d
Mostafa Hisham <52044516+mostafahisham03@users.noreply.github.com>
2023-05-26 03:17:11 +0300

    changed background of beginState and made a folder for backgrounds in res

--- src/views/state/BeginState.java
+++ src/views/state/BeginState.java
@@ -5,12 +5,18 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.Side;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -27,12 +33,15 @@ public class BeginState {
 		root = new VBox();
 		root.setAlignment(Pos.CENTER);
 		root.setSpacing(10);
-		
+		BackgroundImage myBI= new BackgroundImage(new Image("file:./res//backgrounds/game.png",App.WINDOW_WIDTH,App.WINDOW_HEIGHT,false,true),
+		        BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+		          BackgroundSize.DEFAULT);
+		root.setBackground(new Background(myBI));
 		titleLabel = new Label(App.TITLE);
-		titleLabel.setBackground(new Background(new BackgroundFill(Color.YELLOWGREEN, new CornerRadii(5), Insets.EMPTY)));
+        titleLabel.setStyle("-fx-control-inner-background: #000000; -fx-highlight-fill: #dce775; -fx-highlight-text-fill: #000000; -fx-text-fill: #dce775; ");
 		titleLabel.setFont(new Font("Arial", 90));	
 		Button hero = new Button("Choose Your Hero");
-		
+		hero.setStyle("-fx-background-color: #dce775; "); 
 		hero.setPrefSize(200, 50);
 		hero.setOnMouseClicked(new EventHandler<Event>(){
 
@@ -45,9 +54,10 @@ public class BeginState {
 		});
 		
 		Button description = new Button("Description");
+		description.setStyle("-fx-background-color: #dce775; "); 
 		description.setPrefSize(100, 50);
 		description.setOnMouseClicked(new EventHandler<Event>(){
-
+		
 			@Override
 			public void handle(Event arg0) {
 				// TODO Auto-generated method stub
@@ -56,6 +66,7 @@ public class BeginState {
 			
 		});
 		Button quit = new Button("Quit");
+		quit.setStyle("-fx-background-color: #dce775; "); 
 		quit.setPrefSize(100, 50);
 		quit.setOnMouseClicked(new EventHandler<Event>(){
 
